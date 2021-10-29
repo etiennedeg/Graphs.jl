sbromberger/LightGraphs.jl

update connectivity

benchmark/connectivity.jl
@@ -1,12 +1,3 @@
-@benchgroup "connectivity" begin
-  @benchgroup "digraphs" begin
-    for (name, g) in DIGRAPHS
-      @bench "$(name): strongly_connected_components" LightGraphs.strongly_connected_components($g)
-    end
-  end # digraphs
-  @benchgroup "graphs" begin
-    for (name, g) in GRAPHS
-        @bench "$(name): connected_components" LightGraphs.connected_components($g)
-    end
-  end # graphs
-end # connectivity
+suite["centrality"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["graphs"] = @benchmarkable [LightGraphs.connected_components(g) for (n,g) in $GRAPHS]
+suite["centrality"]["digraphs"] = @benchmarkable [LightGraphs.strongly_connected_components(g) for (n,g) in $DIGRAPHS]

etiennedeg/Graphs.jl

benchmark/connectivity.jl
@@ -1,12 +1,3 @@
-@benchgroup "connectivity" begin
-  @benchgroup "digraphs" begin
-    for (name, g) in DIGRAPHS
-      @bench "$(name): strongly_connected_components" Graphs.strongly_connected_components($g)
-    end
-  end # digraphs
-  @benchgroup "graphs" begin
-    for (name, g) in GRAPHS
-        @bench "$(name): connected_components" Graphs.connected_components($g)
-    end
-  end # graphs
-end # connectivity
+suite["centrality"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["graphs"] = @benchmarkable [Graphs.connected_components(g) for (n,g) in $GRAPHS]
+suite["centrality"]["digraphs"] = @benchmarkable [Graphs.strongly_connected_components(g) for (n,g) in $DIGRAPHS]
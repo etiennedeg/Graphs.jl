7a41804c148faa0163ecc4189bb99a6e005e59f8
Ryan Birmingham <birm@rbirm.us>
2021-02-19 11:40:34 -0500

    update connectivity

--- benchmark/connectivity.jl
+++ benchmark/connectivity.jl
@@ -1,12 +1,3 @@
-@benchgroup "connectivity" begin
-  @benchgroup "digraphs" begin
-    for (name, g) in DIGRAPHS
-      @bench "$(name): strongly_connected_components" Graphs.strongly_connected_components($g)
-    end
-  end # digraphs
-  @benchgroup "graphs" begin
-    for (name, g) in GRAPHS
-        @bench "$(name): connected_components" Graphs.connected_components($g)
-    end
-  end # graphs
-end # connectivity
+suite["centrality"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["graphs"] = @benchmarkable [Graphs.connected_components(g) for (n,g) in $GRAPHS]
+suite["centrality"]["digraphs"] = @benchmarkable [Graphs.strongly_connected_components(g) for (n,g) in $DIGRAPHS]
