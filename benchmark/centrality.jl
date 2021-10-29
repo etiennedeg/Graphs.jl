sbromberger/LightGraphs.jl

centrality benchmark migration

benchmark/centrality.jl
@@ -1,26 +1,25 @@
+suite["centrality"] = BenchmarkGroup(["degree", "closeness", "betweenness", "katz", "pagerank"])
 
-@benchgroup "centrality" begin
-  @benchgroup "graphs" begin
-    for (name, g) in GRAPHS
-      @bench "$(name): degree" LightGraphs.degree_centrality($g)
-      @bench "$(name): closeness" LightGraphs.closeness_centrality($g)
-      if nv(g) < 1000
-        @bench "$(name): betweenness" LightGraphs.betweenness_centrality($g)
-        @bench "$(name): katz" LightGraphs.katz_centrality($g)
-      end
-    end
-  end #graphs
-  @benchgroup "digraphs" begin
-    for (name, g) in DIGRAPHS
-      @bench "$(name): degree" LightGraphs.degree_centrality($g)
-      @bench "$(name): closeness" LightGraphs.closeness_centrality($g)
-      if nv(g) < 1000
-        @bench "$(name): betweenness" LightGraphs.betweenness_centrality($g)
-        @bench "$(name): katz" LightGraphs.katz_centrality($g)
-      end
-      if nv(g) < 500
-        @bench "$(name): pagerank"  LightGraphs.pagerank($g)
-      end
-    end
-  end # digraphs
-end # centrality
+# betweenness
+suite["centrality"]["degree"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["degree"]["graphs"] = @benchmarkable [LightGraphs.degree_centrality(g) for (n,g) in $GRAPHS]
+suite["centrality"]["degree"]["digraphs"] = @benchmarkable [LightGraphs.degree_centrality(g) for (n,g) in $DIGRAPHS]
+
+# closeness
+suite["centrality"]["closeness"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["closeness"]["graphs"] = @benchmarkable [LightGraphs.closeness_centrality(g) for (n,g) in $GRAPHS]
+suite["centrality"]["closeness"]["digraphs"] = @benchmarkable [LightGraphs.closeness_centrality(g) for (n,g) in $DIGRAPHS]
+
+# betweenness
+suite["centrality"]["betweenness"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["betweenness"]["graphs"] = @benchmarkable [LightGraphs.betweenness_centrality(g) for (n,g) in $GRAPHS if nv(g) < 1000]
+suite["centrality"]["betweenness"]["digraphs"] = @benchmarkable [LightGraphs.betweenness_centrality(g) for (n,g) in $DIGRAPHS if nv(g) < 1000]
+
+# katz
+suite["centrality"]["katz"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["katz"]["graphs"] = @benchmarkable [LightGraphs.katz_centrality(g) for (n,g) in $GRAPHS if nv(g) < 1000]
+suite["centrality"]["katz"]["digraphs"] = @benchmarkable [LightGraphs.katz_centrality(g) for (n,g) in $DIGRAPHS if nv(g) < 1000]
+
+#pagerank
+suite["centrality"]["katz"] = BenchmarkGroup(["digraphs"])
+suite["centrality"]["katz"]["digraphs"] = @benchmarkable [LightGraphs.pagerank(g) for (n,g) in $DIGRAPHS if nv(g) < 500]

etiennedeg/Graphs.jl

benchmark/centrality.jl
@@ -1,26 +1,25 @@
+suite["centrality"] = BenchmarkGroup(["degree", "closeness", "betweenness", "katz", "pagerank"])
 
-@benchgroup "centrality" begin
-  @benchgroup "graphs" begin
-    for (name, g) in GRAPHS
-      @bench "$(name): degree" Graphs.degree_centrality($g)
-      @bench "$(name): closeness" Graphs.closeness_centrality($g)
-      if nv(g) < 1000
-        @bench "$(name): betweenness" Graphs.betweenness_centrality($g)
-        @bench "$(name): katz" Graphs.katz_centrality($g)
-      end
-    end
-  end #graphs
-  @benchgroup "digraphs" begin
-    for (name, g) in DIGRAPHS
-      @bench "$(name): degree" Graphs.degree_centrality($g)
-      @bench "$(name): closeness" Graphs.closeness_centrality($g)
-      if nv(g) < 1000
-        @bench "$(name): betweenness" Graphs.betweenness_centrality($g)
-        @bench "$(name): katz" Graphs.katz_centrality($g)
-      end
-      if nv(g) < 500
-        @bench "$(name): pagerank"  Graphs.pagerank($g)
-      end
-    end
-  end # digraphs
-end # centrality
+# betweenness
+suite["centrality"]["degree"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["degree"]["graphs"] = @benchmarkable [Graphs.degree_centrality(g) for (n,g) in $GRAPHS]
+suite["centrality"]["degree"]["digraphs"] = @benchmarkable [Graphs.degree_centrality(g) for (n,g) in $DIGRAPHS]
+
+# closeness
+suite["centrality"]["closeness"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["closeness"]["graphs"] = @benchmarkable [Graphs.closeness_centrality(g) for (n,g) in $GRAPHS]
+suite["centrality"]["closeness"]["digraphs"] = @benchmarkable [Graphs.closeness_centrality(g) for (n,g) in $DIGRAPHS]
+
+# betweenness
+suite["centrality"]["betweenness"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["betweenness"]["graphs"] = @benchmarkable [Graphs.betweenness_centrality(g) for (n,g) in $GRAPHS if nv(g) < 1000]
+suite["centrality"]["betweenness"]["digraphs"] = @benchmarkable [Graphs.betweenness_centrality(g) for (n,g) in $DIGRAPHS if nv(g) < 1000]
+
+# katz
+suite["centrality"]["katz"] = BenchmarkGroup(["graphs", "digraphs"])
+suite["centrality"]["katz"]["graphs"] = @benchmarkable [Graphs.katz_centrality(g) for (n,g) in $GRAPHS if nv(g) < 1000]
+suite["centrality"]["katz"]["digraphs"] = @benchmarkable [Graphs.katz_centrality(g) for (n,g) in $DIGRAPHS if nv(g) < 1000]
+
+#pagerank
+suite["centrality"]["katz"] = BenchmarkGroup(["digraphs"])
+suite["centrality"]["katz"]["digraphs"] = @benchmarkable [Graphs.pagerank(g) for (n,g) in $DIGRAPHS if nv(g) < 500]